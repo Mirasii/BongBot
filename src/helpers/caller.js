90e2550ea3a0278bdb7c04f9a866ab05714266eb
Mirasii <meatheat_100mean@hotmail.co.uk>
2024-05-10 23:10:59 +0100

    CALLER wrapper method for callouts

---
+++ src/helpers/caller.js
@@ -0,0 +1,26 @@
+module.exports = {
+    async get(url, path, params, headers) {
+        let fullPath = `${url}${path ? path : ''}${params ? `?${params}`:''}`;
+        console.log(fullPath);
+        let resp = await fetch(fullPath, {
+            method: 'GET',
+            headers: headers
+        }).then(response => {
+            if (!response.ok) { throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${response.text()}`); }
+            return response.json();
+        })
+        return resp;
+    },
+    async post(url, path, headers, body) {
+        let fullPath = url + path;
+        let resp = await fetch(fullPath, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        }).then(response => {
+            if (!response.ok) { throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${response.text()}`); }
+            return response.json();
+        })
+        return resp;
+    }
+}
