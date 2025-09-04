e8ba72b9e28a9e33688869347a0e2b9e66825a2b
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-04 23:17:22 +0100

    ephemerality handling

--- src/helpers/caller.js
+++ src/helpers/caller.js
@@ -1,3 +1,5 @@
+const LOGGER = require(`${__dirname}/logging.js`);
+
 module.exports = {
     async get(url, path, params, headers) {
         let fullPath = `${url}${path ? path : ''}${params ? `?${params}`:''}`;
@@ -12,14 +14,20 @@ module.exports = {
     },
     async post(url, path, headers, body) {
         let fullPath = `${url}${path ? path : ''}`;
+        let text;
         let resp = await fetch(fullPath, {
             method: 'POST',
             headers: headers,
             body: JSON.stringify(body)
-        }).then(response => {
-            if (!response.ok) { throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${response.text()}`); }
+        }).then(async response => {
+            text = await response.text();
+            if (!response.ok) {  
+                throw new Error(`Network response was not ok: ${response.status} ${response.statusText} `); 
+            }
             return response.json();
-        })
+        }).finally(() => {
+            if (text) { LOGGER.log(`${text}`); }
+        });
         return resp;
     }
 }
--- src/helpers/errorBuilder.js
+++ src/helpers/errorBuilder.js
@@ -1,12 +1,16 @@
 const LOGGER = require(`${__dirname}/logging.js`);
-const { MessageFlags } = require('discord.js');
-const errorMsg = 'There was an error while executing the command ';
+const { MessageFlags, EmbedBuilder, Colors } = require('discord.js');
+const errorMsg = 'There was an error while executing the "{interaction}" command.';
 
 function buildError(interaction, error) {
     console.error(`Error executing ${interaction?.commandName ?? 'unknown'} command`);
     LOGGER.log(error);
+    const embed = new EmbedBuilder()
+            .setTitle(errorMsg.replace('{interaction}', interaction?.commandName ?? 'unknown'))
+            .setColor(Colors.Red)
+            .setDescription(error.message ?? 'No additional information provided.');
     return { 
-        content: `${errorMsg}${interaction?.commandName ?? 'unknown'}.`,
+        embeds: [embed.toJSON()],
         flags: MessageFlags.Ephemeral,
         isError: true
     };
