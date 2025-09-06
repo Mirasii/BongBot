7bc83ab50131fea7dca97e2c5a6ce5d755d1a8c6
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-06 01:28:53 +0100

    refactor caller

--- src/helpers/caller.js
+++ src/helpers/caller.js
@@ -2,32 +2,32 @@ const LOGGER = require(`${__dirname}/logging.js`);
 
 module.exports = {
     async get(url, path, params, headers) {
-        let fullPath = `${url}${path ? path : ''}${params ? `?${params}`:''}`;
-        let resp = await fetch(fullPath, {
+        let fullPath = `${url}${path ? path : ''}${params ? `?${params}` : ''}`;
+        let config = {
             method: 'GET',
             headers: headers
-        }).then(response => {
-            if (!response.ok) { throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${response.text()}`); }
-            return response.json();
-        })
-        return resp;
+        };
+        return await makeCallout(fullPath, config);
     },
     async post(url, path, headers, body) {
         let fullPath = `${url}${path ? path : ''}`;
-        let text;
-        let resp = await fetch(fullPath, {
+        let config = {
             method: 'POST',
             headers: headers,
             body: JSON.stringify(body)
-        }).then(async response => {
-            if (!response.ok) { 
-                text = await response.text();
-                throw new Error(`Network response was not ok: ${response.status} ${response.statusText} `); 
-            }
-            return await response.json();
-        }).finally(() => {
-            if (text) { LOGGER.log(`${text}`); }
-        });
-        return resp;
+        };
+        return await makeCallout(fullPath, config);
     }
+}
+
+async function makeCallout(url, config) {
+    let text;
+    let resp = await fetch(url, config).then(async response => {
+        if (response.ok) return await response.json();
+        text = await response.text();
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${text}`);
+    }).finally(() => {
+        if (text) { LOGGER.log(`${text}`); }
+    });
+    return resp;
 }
