0c591d415e13c4f57a3936c3115e67cda9fdac85
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-06 22:44:38 +0100

    fix quotesdb_post around API spec

--- src/commands/quotedb_post.js
+++ src/commands/quotedb_post.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
+const { Colors } = require('discord.js');
 const API = require(`${__dirname}/../config/index.js`).apis.quotedb;
 const CALLER = require(`${__dirname}/../helpers/caller.js`);
 const ERROR_BUILDER = require(`${__dirname}/../helpers/errorBuilder.js`);
@@ -13,24 +14,26 @@ module.exports = {
         try {
             const quote = interaction.options.getString('quote');
             const author = interaction.options.getString('author');
-            console.log(interaction.guild.id);
-            console.log(interaction.guild.name);
             const response = await CALLER.post(
                 API.url,
-                '/api/v1/add_quote',
+                '/api/v1/quotes',
                 { 'Content-Type': 'application/json', 'Authorization': `Bearer ${API.apikey}` },
                 { 
                     quote: quote, 
                     author: author,
-                    user:  {
-                        id: interaction.guild.id,
-                        name: interaction.guild.name,
-                    },
-                    owner_id: API.owner_id,
+                    user_id: API.user_id,
                     date: new Date().toLocaleString()
                 }
             );
-            return `Quote Successfully Added:\n*"${response}"*`;
+            const embed = new EmbedBuilder()
+                    .setTitle(`📜 New Quote Created`)
+                    .setColor(Colors.Purple)
+                    .addFields([
+                        { name:  `*"${response.quote.quote}"*`, value: `🪶 - ${response.quote.author}`, inline: false },
+                    ])
+                    .setFooter({ text: `BongBot • Quotes from dev.elmu.db`, iconURL: client.user.displayAvatarURL() })
+                    .setTimestamp();
+            return { embeds: [embed] };
         } catch (error) {
             return await ERROR_BUILDER.buildError(interaction, error);
         }
