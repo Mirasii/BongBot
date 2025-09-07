d50fc6af08454e3f35c06d08dd8a44c555516313
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-07 21:15:45 +0100

    /arab test

---
+++ tests/commands/arab.test.js
@@ -0,0 +1,28 @@
+
+const arabCommand = require('../../src/commands/arab');
+const { SlashCommandBuilder } = require('discord.js');
+
+describe('arab command', () => {
+    it('should have a data property', () => {
+        expect(arabCommand.data).toBeInstanceOf(SlashCommandBuilder);
+    });
+
+    it('should have a name of "arab"', () => {
+        expect(arabCommand.data.name).toBe('arab');
+    });
+
+    it('should have a description', () => {
+        expect(arabCommand.data.description).toBeTruthy();
+    });
+
+    it('should have an execute method', () => {
+        expect(arabCommand.execute).toBeInstanceOf(Function);
+    });
+
+    it('should return the correct file object', async () => {
+        const result = await arabCommand.execute();
+        expect(result).toHaveProperty('files');
+        expect(result.files[0]).toHaveProperty('attachment');
+        expect(result.files[0].name).toBe('arab.mp4');
+    });
+});
