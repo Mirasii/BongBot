f99adf9c7f6e1aef76bd2e9205f8ef2a04308a70
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-09-07 21:23:21 +0100

    creeper test

---
+++ tests/commands/creeper.test.js
@@ -0,0 +1,28 @@
+
+const creeperCommand = require('../../src/commands/creeper');
+const { SlashCommandBuilder } = require('discord.js');
+
+describe('creeper command', () => {
+    it('should have a data property', () => {
+        expect(creeperCommand.data).toBeInstanceOf(SlashCommandBuilder);
+    });
+
+    it('should have a name of "creeper"', () => {
+        expect(creeperCommand.data.name).toBe('creeper');
+    });
+
+    it('should have a description', () => {
+        expect(creeperCommand.data.description).toBeTruthy();
+    });
+
+    it('should have an execute method', () => {
+        expect(creeperCommand.execute).toBeInstanceOf(Function);
+    });
+
+    it('should return the correct file object', async () => {
+        const result = await creeperCommand.execute();
+        expect(result).toHaveProperty('files');
+        expect(result.files[0]).toHaveProperty('attachment');
+        expect(result.files[0].name).toBe('creeper.mp4');
+    });
+});
