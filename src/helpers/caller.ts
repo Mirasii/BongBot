429090affa790b7bd81576519ef0fe520ad5c3b9
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-11-05 20:18:06 +0000

    update caller

---
+++ src/helpers/caller.ts
@@ -0,0 +1,34 @@
+import LOGGER from './logging'
+
+export async function get(url: string, path?: string, params?: string, headers?: { [key: string]: any }) {
+    const config = {
+        method: 'GET',
+        headers: headers
+    };
+    return await makeCallout(constructFullPath(url, path, params), config);
+}
+export async function post(url: string, path?: string, headers?: { [key: string]: any }, body?: any) {
+    const config = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+    return await makeCallout(constructFullPath(url, path), config);
+}
+
+
+function constructFullPath(url: string, path?: string, params?: string) {
+    return `${url}${path ?? ''}${params ? `?${params}` : ''}`;
+}
+
+async function makeCallout(url: string, config: { [key: string]: any }): Promise<any> {
+    let text: string | null;
+    let resp = await fetch(url, config).then(async response => {
+        if (response.ok) return await response.json();
+        text = await response.text();
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText} ${text}`);
+    }).finally(() => {
+        if (text) { LOGGER.log(`${text}`); }
+    });
+    return resp;
+}
