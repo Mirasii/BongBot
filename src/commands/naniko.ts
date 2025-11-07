9ad065a88183db3f0e7bca5978241ae4b5ca96da
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-11-07 02:24:28 +0000

    swap node-cron to node-schedular (compatability)

--- src/commands/naniko.ts
+++ src/commands/naniko.ts
@@ -1,7 +1,7 @@
 import { TikTokLiveConnection } from 'tiktok-live-connector';
 import { ExtendedClient } from '../helpers/interfaces.js';
 import { EmbedBuilder, Colors } from 'discord.js';
-import cron from 'node-cron';
+import cron from 'node-schedule';
 
 const tiktok_username = 'pokenonii';
 const streamer_avatar = 'https://p19-common-sign-useastred.tiktokcdn-eu.com/tos-useast2a-avt-0068-euttp/ab8fb67e6366564545d1519484e2b2f0~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=10399&refresh_token=4d76bc58&x-expires=1762650000&x-signature=Zmkwn4lhOimtFRVXmVBNYvj3y0A%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=no1a';
@@ -22,12 +22,13 @@ export default class TikTokLiveNotifier {
                         { name: '⏱️ Naniko Noni is live!', value: `[Watch the stream here!](https://www.tiktok.com/@pokenonii/live)`, inline: false },
                     )
                     .setFooter({ text: `BongBot • ${this.#client.version}`, iconURL: this.#client.user?.displayAvatarURL() })
-        cron.schedule('0 15 * * *', () => {
+        cron.scheduleJob('0 15 * * *', () => {
             this.#checkLive();
         });
     }
 
     async #checkLive(): Promise<void> {
+        console.log('checklive entered');
         await this.#connector.waitUntilLive(10800)
         .then(async(state) => {
             if (!process.env.TIKTOK_LIVE_CHANNEL_ID) { return; }
