7343ef35e0e0e39f03cc4d4c5beb9922690f7b59
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-12-05 17:43:21 +0000

    update to add contexts

--- src/commands/quotedb_get.ts
+++ src/commands/quotedb_get.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client, InteractionContextType  } from 'discord.js';
 import QuoteBuilder from '../helpers/quoteBuilder.js';
 import { buildError } from '../helpers/errorBuilder.js';
 
@@ -7,7 +7,8 @@ export default {
         .setName('get_quotes')
         .setDescription('Get up to 5 recent quotes!')
         .addIntegerOption(option => option.setName('number').setDescription('How many quotes do you want?').setRequired(false))
-        .addBooleanOption(option => option.setName('server').setDescription('Get quotes from this server only? Default: true').setRequired(false)),
+        .addBooleanOption(option => option.setName('server').setDescription('Get quotes from this server only? Default: true').setRequired(false))
+        .setContexts([InteractionContextType.Guild]),
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
         try {
             return new QuoteBuilder().getQuote('/api/v1/quotes/search', 'Recent Quotes', client, interaction);
--- src/commands/quotedb_get_random.ts
+++ src/commands/quotedb_get_random.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, Client, ChatInputCommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, Client, ChatInputCommandInteraction, InteractionContextType } from 'discord.js';
 import QuoteBuilder from '../helpers/quoteBuilder.js';
 import { buildError } from '../helpers/errorBuilder.js';
 
@@ -6,7 +6,9 @@ export default {
     data: new SlashCommandBuilder()
         .setName('random_quotes')
         .setDescription('Get up to 5 random quotes!')
-        .addIntegerOption(option => option.setName('number').setDescription('How many quotes do you want?').setRequired(false)),
+        .addIntegerOption(option => option.setName('number').setDescription('How many quotes do you want?').setRequired(false))
+        .addBooleanOption(option => option.setName('server').setDescription('Get quotes from this server only? Default: true').setRequired(false))
+        .setContexts([InteractionContextType.Guild]),
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
         try {
             return new QuoteBuilder().getQuote('/api/v1/quotes/random', 'Random Quotes', client, interaction);
