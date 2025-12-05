1a8df8636662439114b6eaa9aacc2e52433e05ef
Mirasii <meatheat_100mean@hotmail.co.uk>
2025-12-05 17:27:43 +0000

    update quotedb_get to use new get handling from QuoteBuilder

--- src/commands/quotedb_get.ts
+++ src/commands/quotedb_get.ts
@@ -1,32 +1,16 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from 'discord.js';
 import QuoteBuilder from '../helpers/quoteBuilder.js';
-import { apis } from '../config/index.js';
-import CALLER from '../helpers/caller.js';
 import { buildError } from '../helpers/errorBuilder.js';
-
-const API = apis.quotedb;
 
 export default {
     data: new SlashCommandBuilder()
         .setName('get_quotes')
         .setDescription('Get up to 5 recent quotes!')
-        .addIntegerOption(option => option.setName('number').setDescription('How many quotes do you want?').setRequired(false)),
+        .addIntegerOption(option => option.setName('number').setDescription('How many quotes do you want?').setRequired(false))
+        .addBooleanOption(option => option.setName('server').setDescription('Get quotes from this server only? Default: true').setRequired(false)),
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
         try {
-            const number = interaction.options.getInteger('number') ?? 1;
-            if (number > 5) return await buildError(interaction, new Error("You can only request up to 5 quotes at a time."));
-
-            const response = await CALLER.get(
-                API.url,
-                `/api/v1/quotes/search/user/${API.user_id}`,
-                `max_quotes=${number}`,
-                { 'Content-Type': 'application/json', 'Authorization': `Bearer ${API.apikey}` },
-            );
-            if (response?.quotes?.length === 0) return await buildError(interaction, new Error("No quotes found."));
-            return new QuoteBuilder()
-                .setTitle('Recent Quotes')
-                .addQuotes(response.quotes)
-                .build(client);
+            return new QuoteBuilder().getQuote('/api/v1/quotes/search', 'Recent Quotes', client, interaction);
         } catch (error) {
             return await buildError(interaction, error);
         }
