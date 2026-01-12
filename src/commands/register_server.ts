4539265cae0c5f080a068fe5e920be8c5cb834de
Mirasii <meatheat_100mean@hotmail.co.uk>
2026-01-12 03:05:10 +0000

    add register server command

---
+++ src/commands/register_server.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from 'discord.js';
+import { buildError } from '../helpers/errorBuilder.js';
+import Database from '../helpers/database.js';
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('register_server')
+        .setDescription('Register a new pterodactyl server to the bot.')
+        .addStringOption(option => option.setName('server_url').setDescription('The URL of the pterodactyl panel.').setRequired(true))
+        .addStringOption(option => option.setName('api_key').setDescription('The API key for the pterodactyl panel.').setRequired(true))
+        .addStringOption(option => option.setName('server_name').setDescription('The Name of the server to register.').setRequired(true)),
+    async execute(interaction: ChatInputCommandInteraction, client: Client) {
+        try {
+            const db = new Database(process.env.SERVER_DATABASE || 'pterodactyl.db');
+            const serverUrl = interaction.options.getString('server_url', true);
+            const apiKey = interaction.options.getString('api_key', true);
+            const serverName = interaction.options.getString('server_name', true);
+            const userId = interaction.user.id;
+
+            const serverId = db.addServer({
+                userId,
+                serverName,
+                serverUrl,
+                apiKey
+            });
+
+            db.close();
+
+            await interaction.reply({
+                content: `Successfully registered server **${serverName}** (ID: ${serverId})!\nURL: ${serverUrl}`,
+                ephemeral: true
+            });
+        } catch (error) {
+            return await buildError(interaction, error);
+        }
+    },
+    fullDesc: {
+        options: [],
+        description: "Registers a new pterodactyl server to the bot."
+    }
+}
