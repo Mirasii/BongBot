6ee51a25b119aeddbe10f64a2531029f44c83978
Mirasii <meatheat_100mean@hotmail.co.uk>
2026-01-18 05:35:54 +0000

    update logging.ts comment

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -14,7 +14,10 @@ export default {
     get default(): Logger {
         return DatabasePool.getInstance().getLoggerConnection();
     },
-    /** Legacy log function has been updated to use the new _logger so that code uses it implicitly. */
+    /** 
+     * Legacy log function has been updated to use the new _logger so that code uses it implicitly. 
+     * Old code using LOGGER.log(error) will still work as expected, however it is recommended to use the new Logger interface directly.
+     */
     async log(error: any) {
         const _logger = DatabasePool.getInstance().getLoggerConnection();
         if (!_logger) throw new Error('Logger not initialised');
